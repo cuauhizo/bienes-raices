f08a3a7d25b2a9dc40b02929f630aec9694f65fc
Fabian Rodriguez Cuauhizo <cuauhizo@gmail.com>
2024-02-16 18:16:29 -0600

    funcionalidad login y agregar propiedad

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router"
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
 </script>
 
 <template>
@@ -12,12 +15,14 @@ import { RouterLink, RouterView } from "vue-router";
           </v-btn>
         </template>
         <template v-slot:append>
-          <v-btn :to="{ name: 'home' }">
-            Inicio
-          </v-btn>
-          <v-btn :to="{ name: 'login' }">
-            Iniciar Sesión
-          </v-btn>
+          <div v-if="auth.isAuth">
+            <v-btn :to="{ name: 'admin-propiedades' }"> Admin </v-btn>
+            <v-btn @click="auth.logout" > Cerrar Sesión </v-btn>
+          </div>
+          <div v-else>
+            <v-btn :to="{ name: 'home' }"> Inicio </v-btn>
+            <v-btn :to="{ name: 'login' }"> Iniciar Sesión </v-btn>
+          </div>
         </template>
       </v-app-bar>
       <v-main>
---
+++ src/composables/useImage.js
@@ -0,0 +1,35 @@
+import { computed } from 'vue'
+import { ref as storageRef } from 'firebase/storage'
+import { useFirebaseStorage, useStorageFile } from 'vuefire'
+import { uid } from 'uid'
+
+export default function useImage() {
+
+  const storage = useFirebaseStorage()
+  const storageRefPath = storageRef(storage, `/propiedades/${uid()}.jpg`)
+
+  const {
+    url,
+    upload
+  } = useStorageFile(storageRefPath)
+
+  function uploadImage(e) {
+    // console.log(e.target.files[0])
+    const data = e.target.files[0]
+    if (data) {
+      upload(data)
+    }
+    console.log(url);
+  }
+
+  const image = computed(() => {
+    return url.value ? url.value : null
+  })
+
+  return {
+    url,
+    uploadImage,
+    image
+  }
+
+}
